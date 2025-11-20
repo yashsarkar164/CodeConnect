81c6d3837941f1d5acc6cf80866359df36aefb50
YASH sarkar <132969742+yashsarkar164@users.noreply.github.com>
2025-11-20 17:48:11 +0530

    env

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,23 +1,8 @@
 import axios from "axios";
-import { getAuth } from "@clerk/clerk-react";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true,
-});
-
-// Attach Clerk JWT token before each request
-axiosInstance.interceptors.request.use(async (config) => {
-  const { userId, getToken } = getAuth();
-
-  if (userId) {
-    const token = await getToken({ template: "default" });
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-  }
-
-  return config;
+  withCredentials: true, // by adding this field browser will send the cookies to server automatically, on every single req
 });
 
 export default axiosInstance;
